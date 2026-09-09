a75c8600a8b47d7eff7a332b013b0d0886084405
lolabug <wambolt.w5756@gmail.com>
2026-09-08 22:46:47 -0400

    did some stuff

--- pages/random_calcs.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("Random Calcs")
-if st.button("Home"):
-    st.switch_page("pages/home.py")
-col1, col2, col3 = st.columns(3)
-with col2:
-    if st.button("BloxFruits Mastery Calc"):
-        st.switch_page("pages/bloxfruits_mastery_calc.py")
